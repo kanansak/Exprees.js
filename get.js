8de626b0dc9a9c54af41fea3a70074e534fb7482
Thoth <kanansak555@gmail.com>
2023-09-28 10:51:24 +0700

    Update get.js

--- get.js
+++ get.js
@@ -1,22 +1,19 @@
 const express = require('express');
+const db = require('./db');
 const router = express.Router();
 
 // เส้นทาง GET '/get-route'
 router.get('/get-route', (req, res) => {
-  res.send('นี่คือเส้นทาง GET ในไฟล์ get.js');
+  //res.send('นี่คือเส้นทาง GET ในไฟล์ get.js');
+    // ทำคำสั่ง SQL ของคุณที่นี่และส่งผลลัพธ์กลับไปให้กับผู้ใช้
+    db.query('SELECT * FROM ESP_DATA', (err, results) => {
+      if (err) {
+        console.error('เกิดข้อผิดพลาดในการสอบถามฐานข้อมูล: ' + err.message);
+        res.status(500).send('เกิดข้อผิดพลาดในการสอบถามฐานข้อมูล');
+      } else {
+        res.json(results); // ส่งข้อมูลในรูปแบบ JSON กลับไปให้กับผู้ใช้
+      }
+    });
 });
-router.get("/read", async (req, res) => {
-    try {
-        connection.query("SELECT * FROM ESP_DATA", (err, results, fields) => {
-            if (err) {
-                console.log(err);
-                return res.status(400).send();
-            }
-            res.status(200).json(results)
-        })
-    } catch(err) {
-        console.log(err);
-        return res.status(500).send();
-    }
-})
+
 module.exports = router;
