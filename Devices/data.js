44bce97963d8402576d0269f0be0dd913ac6d0d3
Thoth <kanansak555@gmail.com>
2023-10-11 13:27:51 +0700

    Update CRUD

--- Devices/data.js
+++ Devices/data.js
@@ -32,34 +32,38 @@ router.get('/data_tuya', (req, res) => {
   });
 });
 
-// GET Latest Data_ESP
 router.get('/latest_data_esp', (req, res) => {
-    const query = 'SELECT * FROM Data_ESP ORDER BY created_timestamp DESC LIMIT 1';
-    
-    db.query(query, (err, result) => {
-      if (err) {
-        console.error('เกิดข้อผิดพลาดในการดึงข้อมูลล่าสุด Data_ESP: ' + err.message);
-        res.status(500).json({ message: 'เกิดข้อผิดพลาดในการดึงข้อมูลล่าสุด Data_ESP' });
-        return;
-      }
-  
-      res.json(result[0]); // รายการแรกคือข้อมูลล่าสุด
+    const device_id = req.query.device_id; // รับ device_id จากคำขอ HTTP
+    const query = 'SELECT * FROM Data_ESP WHERE device_id = ? ORDER BY created_timestamp DESC LIMIT 1';
+
+    db.query(query, [device_id], (err, result) => {
+        if (err) {
+            console.error('เกิดข้อผิดพลาดในการดึงข้อมูลล่าสุด Data_ESP: ' + err.message);
+            res.status(500).json({ message: 'เกิดข้อผิดพลาดในการดึงข้อมูลล่าสุด Data_ESP' });
+            return;
+        }
+
+        res.json(result[0]); // รายการแรกคือข้อมูลล่าสุด
     });
-  });
-  
-  // GET Latest Data_Tuya
-  router.get('/latest_data_tuya', (req, res) => {
-    const query = 'SELECT * FROM Data_Tuya ORDER BY created_timestamp DESC LIMIT 1';
-    
-    db.query(query, (err, result) => {
-      if (err) {
-        console.error('เกิดข้อผิดพลาดในการดึงข้อมูลล่าสุด Data_Tuya: ' + err.message);
-        res.status(500).json({ message: 'เกิดข้อผิดพลาดในการดึงข้อมูลล่าสุด Data_Tuya' });
-        return;
-      }
-  
-      res.json(result[0]); // รายการแรกคือข้อมูลล่าสุด
+});
+
+router.get('/latest_data_tuya', (req, res) => {
+    const device_id = req.query.device_id; // รับ device_id จากคำขอ HTTP
+    const query = 'SELECT * FROM Data_Tuya WHERE device_id = ? ORDER BY created_timestamp DESC LIMIT 1';
+
+    db.query(query, [device_id], (err, result) => {
+        if (err) {
+            console.error('เกิดข้อผิดพลาดในการดึงข้อมูลล่าสุด Data_Tuya: ' + err.message);
+            res.status(500).json({ message: 'เกิดข้อผิดพลาดในการดึงข้อมูลล่าสุด Data_Tuya' });
+            return;
+        }
+
+        res.json(result[0]); // รายการแรกคือข้อมูลล่าสุด
     });
-  });
+});
+
   
 module.exports = router;
+//latest_data_esp?device_id=your_device_id
+//latest_data_tuya?device_id=your_device_id
+//http://localhost:3000/latest_data_esp?device_id=ESP-001
--- server.js
+++ server.js
@@ -10,35 +10,29 @@ app.use(cors());
 app.use(express.json());
 
 
-// นำเส้นทาง GET จากไฟล์ get.js
 const getRoutes = require('./Devices/latestData');
 app.use(getRoutes);
-
-// นำเส้นทาง POST จากไฟล์ post.js
-const postRoutes = require('./post');
-app.use(postRoutes); // กำหนดเส้นทางที่จะเชื่อมกับ postRoutes ในที่นี้คือ '/post-route'
 
 
 // นำเส้นทาง CRUD ของผู้ใช้งานมาใช้
  const userRoutes = require('./User/api-user'); // นำเข้าไฟล์ CRUD ของผู้ใช้งาน
  app.use(userRoutes);
 
-//http://localhost:3000/api/data
-const dataRouter = require('./api');
-app.use('/api', dataRouter);
 
 //http://localhost:3000/devices/
-const deviceRouter = require('./Devices/api-device')
-app.use(deviceRouter);
+//const deviceRouter = require('./Devices/api-device')
+//app.use(deviceRouter);
 
+
+//http://localhost:3000/test/devices
 const datadevice = require('./Devices/data-device')
 app.use('/test',datadevice);
 
+
+//http://localhost:3000/data_esp
 const data =require('./Devices/data')
 app.use(data);
 
-const auth0 = require('./User/auth0')
-app.use(auth0);
 
 // รัน Express server ที่พอร์ตที่คุณต้องการ
 const PORT = process.env.PORT || 3000;
